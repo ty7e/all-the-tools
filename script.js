83f22fbc602e59b6ed7806b23bc614931ec4d90e
ty7e <61668943+ty7e@users.noreply.github.com>
2024-09-15 19:54:24 +0200

    Show more info

--- script.js
+++ script.js
@@ -12,21 +12,32 @@ function listData() {
 };
 
 function catHeader(catName) {
-	const header = document.createElement('h2');
+	const header = document.createElement("h1");
 	header.innerHTML = catName;
 	document.body.appendChild(header);
 }
 
 function listCat(cat) {
-	const ul = document.createElement('ul');
+	const ul = document.createElement("ul");
 	ul.classList.add("tool-list");
 	document.body.appendChild(ul);
 	
 	cat.forEach((tool) => {
 		
 		let li = document.createElement("li");
-        li.innerText = tool.name;
-		li.classList.add("tool-item", "hidden");
+		li.classList.add("tool-list-item", "hidden");
+		
+		const name = document.createElement("div");
+		name.innerHTML = tool.name;
+		name.style.fontWeight = "bold";
+		const legend = document.createElement("div");
+		legend.innerHTML = tool.legend;
+		const url = document.createElement("div");
+		const urlLink = document.createElement("a");
+		urlLink.text = "site";
+		urlLink.href = tool.url;
+		url.appendChild(urlLink);
+		li.append(name, legend, url);
 		
 		ul.appendChild(li);
 		window.getComputedStyle(li).opacity;
